112e50e54ca954add812f48a3b9235e5a4803ad6
Humna <humna.awan@rutgers.edu>
2022-11-12 20:14:02 -0800

    updated spectra plotting functions (e.g. now include plots for ivar; details about objs).

--- scripts/helpers_plots.py
+++ scripts/helpers_plots.py
@@ -17,37 +17,81 @@ __all__ = ['compare_spectra_template_vs_calibrated',
            ]
 
 # ------------------------------------------------------------------------
-# function to plot template spectra and calibration spectra (From the Spectra files and the cframe files)
 def compare_spectra_template_vs_calibrated(exposures_path, night, expid,
-                                           showfig=True, savefig=False, outdir=None, simspec_ind=None):
-    path = f'{exposures_path}/exposures/{night}/{expid}'
-    data_simspec = Table.read(f'{path}/simspec-{expid}.fits', format='fits')
-    simspec_hdul = fits.open(f'{path}/simspec-{expid}.fits')
+                                           showfig=True,
+                                           savefig=False, outdir=None,
+                                           simspec_ind=None):
+    """
+    this function plots template spectra and calibration spectra
+    (from both Spectra and cframe files), alongside the inverse
+    variance from the two files.
+
+    required inputs
+    ---------------
+    * exposures_path: str: path to the exposures folder (i.e. where subfolder
+                           names are night numbers, which contain expid folders)
+    * night: str or int: night to consider
+    * expid: str or int: exposure id to consider
+
+    optional parameters
+    -------------------
+    * showfig: bool: set to True to show the figure
+                     default: True
+    * savefig: bool: set to True to save the figure
+                     default: Fakse
+    * outdir: str: directory where the plot is to be saved; applies when
+                   savefig = True.
+                   default: None
+    * simspec_ind: int: index in simspec file to plot; otherwise a random
+                        object is chosen.
+                        default: None
+
+    """
+    print('## running compare_spectra_template_vs_calibrated .. ')
+    if savefig and outdir is None:
+        raise ValueError('## outdir must be specified if savefig = True')
+
+    path = f'{exposures_path}/{night}/{expid}'
+    fname = f'{path}/simspec-{expid}.fits'
+    if os.path.exists(fname):
+        simspec_hdul = fits.open(fname)
+        data_simspec_truth = Table(simspec_hdul[simspec_hdul.index_of('TRUTH')].data)
+    else:
+        print(f'## {fname} not found. exiting.')
+        return
     data_fibermap = Table.read(f'{path}/fibermap-{expid}.fits', format='fits')
-    
+
     # print out some stats
-    print(f'\nkeys in the sim-spec file: {data_simspec.keys()}\n')
+    print(f'\nkeys in the sim-spec file: {data_simspec_truth.keys()}\n')
     for key in ['TRUESPECTYPE', 'TEMPLATETYPE', 'TEMPLATESUBTYPE']:
-        print(f'{key}: {np.unique(data_simspec[key].value.data, return_counts=True)}')
+        print(f'{key}: {np.unique(data_simspec_truth[key].value.data, return_counts=True)}')
 
     print(f'\nkeys in the fibermap file: {data_fibermap.keys()}\n')
-    
+
     # lets now plot the spectrum of a random object
     if simspec_ind is None:
-        simspec_ind = random.choice(range(len(data_simspec)))
-        
+        simspec_ind = random.choice(range(len(data_simspec_truth)))
+
     # sanity check
-    if data_simspec['TARGETID'][simspec_ind] != data_fibermap['TARGETID'][simspec_ind]:
+    if data_simspec_truth['TARGETID'][simspec_ind] != data_fibermap['TARGETID'][simspec_ind]:
         raise ValueError(f'simspec and fibermaps indices do NOT match: simspec-index = {simspec_ind}')
     else:
-        targetid = data_simspec['TARGETID'][simspec_ind]
-        
+        targetid = data_simspec_truth['TARGETID'][simspec_ind]
+
     # first plot the simspec spectrum
+    wave_ind = simspec_hdul.index_of('WAVE')
+    flux_ind = simspec_hdul.index_of('FLUX')
+
     plt.clf()
-    fig, axes = plt.subplots(2, 1)
-    axes[0].plot(simspec_hdul[0].data, simspec_hdul[1].data[simspec_ind, :], 'kx-', label='simspec spectrum')
-    axes[1].plot(simspec_hdul[0].data, simspec_hdul[1].data[simspec_ind, :], 'kx-', label='simspec spectrum')
-    
+    nrows, ncols = 2, 2
+    fig, axes = plt.subplots(nrows, ncols)
+    axes[0, 0].plot(simspec_hdul[wave_ind].data,
+                 simspec_hdul[flux_ind].data[simspec_ind, :].reshape(len(simspec_hdul[wave_ind].data)),
+                 'kx-', label='simspec spectrum')
+    axes[0, 1].plot(simspec_hdul[wave_ind].data,
+                 simspec_hdul[flux_ind].data[simspec_ind, :].reshape(len(simspec_hdul[wave_ind].data)),
+                 'kx-', label='simspec spectrum')
+
     # now the calibrated one - from the Spectra file
     # lets now extract the petal info
     petal_ind = data_fibermap['PETAL_LOC'][simspec_ind]
@@ -55,28 +99,34 @@ def compare_spectra_template_vs_calibrated(exposures_path, night, expid,
     ind_spectra = np.where(data_spectra.fibermap['TARGETID'] == targetid)[0]
     for band in 'brz':
         nwave = len(data_spectra.wave[band])
-        axes[0].plot(data_spectra.wave[band], data_spectra.flux[band][ind_spectra, :].reshape(nwave),
+        axes[0, 0].plot(data_spectra.wave[band], data_spectra.flux[band][ind_spectra, :].reshape(nwave),
                      label=f'calibrated: Spectra: {band}')
+        axes[1, 0].plot(data_spectra.wave[band], data_spectra.ivar[band][ind_spectra, :].reshape(nwave),
+                      label=f'calibrated: Spectra: {band}')
 
     # and now the calibrated one - from the cframe files
-    for band in 'brz': 
+    # also plot the ivar
+    wave_all, flux_all = [], []
+    for band in 'brz':
         data_frame = read_frame(f'{path}/cframe-{band}{petal_ind}-{expid}.fits')
         ind_frame = np.where(data_frame.fibermap['TARGETID'] == targetid)[0]
-        axes[1].plot(data_frame.wave, data_frame.flux[ind_frame][0, :], label=f'calibrated: cframe: {band}')
-    
+        axes[0, 1].plot(data_frame.wave, data_frame.flux[ind_frame][0, :], label=f'calibrated: cframe: {band}')
+        axes[1, 1].plot(data_frame.wave, data_frame.ivar[ind_frame][0, :], label=f'calibrated: cframe: {band}')
+
     # set up the title
     title = ''
-    for key in ['TRUESPECTYPE', 'TEMPLATETYPE', 'TEMPLATESUBTYPE']:
-        title += f'{key} {data_simspec[key][simspec_ind]} ; '
+    for key in ['TRUEZ', 'TRUESPECTYPE', 'TEMPLATETYPE', 'TEMPLATESUBTYPE']:
+        title += f'{key} {data_simspec_truth[key][simspec_ind]} ; '
     title = title[0:-2]
     plt.suptitle(f'night {night} ; expid {expid}\n' +
-                 f'target ID {targetid} ; petal {petal_ind} ; simspec-ind {simspec_ind}\n{title}', y=0.99)
-    # pot details
-    for ax in axes:
-        ax.legend(bbox_to_anchor=(1,1))
-        ax.set_ylabel('flux')
-    axes[1].set_xlabel('wavelength')
-    fig.set_size_inches(10, 6)
+                 f'target ID {targetid} ; petal {petal_ind} ; simspec-ind {simspec_ind}\n{title}', y=1.15)
+    # plot details
+    for ncol in range(ncols):
+        axes[0, ncol].set_ylabel('flux')
+        axes[1, ncol].set_ylabel('ivar')
+        axes[-1, ncol].set_xlabel('wavelength')
+        axes[0, ncol].legend(bbox_to_anchor=(0.93, 1.4), ncol=2)
+    fig.set_size_inches(10*ncols, 6)
     # save plot and/or show
     if showfig:
         plt.show()
@@ -87,57 +137,108 @@ def compare_spectra_template_vs_calibrated(exposures_path, night, expid,
         print('## saved %s' % fname )
 
 # ------------------------------------------------------------------------
-# function to compare ungrouped/uncoadded spectra with grouped/coadded ones
-def compare_spectra_groupedcoadded_vs_not(exposures_path, nside,
-                                          showfig=True, savefig=False, outdir=None, hpixnum=None, targetid=None):
-    path = f'{exposures_path}/exposures/spectra-{nside}'
+def compare_spectra_groupedcoadded_vs_not(exposures_path, coadd_spectra_path,
+                                          nside, showfig=True,
+                                          savefig=False, outdir=None,
+                                          hpixnum=None, targetid=None,
+                                          return_objdata=False):
+    """
+    this function creates plots to compare template spectra vs ungrouped
+    calibrated spectra vs. grouped/coadded ones.
+
+    required inputs
+    ---------------
+    * exposures_path: str: path to the exposures folder (i.e. where subfolder
+                           names are night numbers, which contain expid folders)
+    * coadd_spectra_path: str: path to the spectra-<nside> folder.
+    * nside: int: HEALPix resolution parameter
+
+    optional parameters
+    -------------------
+    * showfig: bool: set to True to show the figure
+                     default: True
+    * savefig: bool: set to True to save the figure
+                     default: Fakse
+    * outdir: str: directory where the plot is to be saved; applies when
+                   savefig = True.
+                   default: None
+    * hpixnum: int: hpixnum to consider; otherwise a random one is chosen.
+                    default: None
+    * targetid: int: targetid to consider; otherwise a random one is chosen
+                     the given hpix.
+                     default: None
+    * return_objdata: bool: set to True to get simspec_ind, night, expid
+                            for the object in the plots.
+                            default: False
+
+    """
+    print('## running compare_spectra_groupedcoadded_vs_not ..')
+    if savefig and outdir is None:
+        raise ValueError('## outdir must be specified if savefig = True')
     # pick one of the healpix numbers randomly
     if hpixnum is None:
-        hpixnum = int(random.choice(os.listdir(path)))
+        # gather available pixels
+        hpixels = []
+        for subdir in os.listdir(coadd_spectra_path):
+            hpixels += os.listdir(f'{coadd_spectra_path}/{subdir}')
+
+        hpixnum = int(random.choice(hpixels))
     # now read the grouped data
-    grouped = read_spectra(f'{path}/{hpixnum}/grouped-{nside}-{hpixnum}.fits')
-    
+    grouped = read_spectra(f'{coadd_spectra_path}/{hpixnum//100}/{hpixnum}/grouped-{nside}-{hpixnum}.fits')
+
     # now pick a random targetid
     if targetid is None:
         targetid = random.choice(np.unique(grouped.fibermap['TARGETID'].value))
-    
+        print(f'## targetid = {targetid}')
+
     ind_grouped = np.where(grouped.fibermap['TARGETID'].value == targetid)[0]
     # now plot things out
     nrows, ncols = len(ind_grouped), 4
     print(f'{nrows} spectra found to be grouped')
-    fig, axes = plt.subplots(nrows, ncols)
+    fig1, axes1 = plt.subplots(nrows, ncols)
     plt.subplots_adjust(hspace=0.6, wspace=0.25)
-    
+
+    fig2, axes2 = plt.subplots(nrows, ncols)
+    plt.subplots_adjust(hspace=0.6, wspace=0.25)
+
     for i, ind in enumerate(ind_grouped):
         # lets plot the grouped spectra
         for band in 'brz':
             nwave = len(grouped.wave[band])
             if nrows == 1:
-                ax = axes[2]
+                ax1 = axes1[2]
+                ax2 = axes2[2]
             else:
-                ax = axes[i, 2]
-            ax.plot(grouped.wave[band], grouped.flux[band][ind, :].reshape(nwave))
-            ax.set_title(f'grouped observation; ind {ind}\nflux from grouped-spectra file', fontsize=14)
-            
+                ax1 = axes1[i, 2]
+                ax2 = axes2[i, 2]
+            ax1.plot(grouped.wave[band], grouped.flux[band][ind, :].reshape(nwave))
+            ax1.set_title(f'grouped observation; ind {ind}\nflux from grouped-spectra file', fontsize=14)
+
+            ax2.plot(grouped.wave[band], grouped.ivar[band][ind, :].reshape(nwave))
+            ax2.set_title(f'grouped observation; ind {ind}\nivar from grouped-spectra file', fontsize=14)
+
         # now plot the spectra file from disc that were presumbly used to create the grouped spectra
         petal = grouped.fibermap[ind]['PETAL_LOC']
         night, expid = grouped.fibermap['NIGHT'][ind], grouped.fibermap['EXPID'][ind]
         expid = f'00000{expid}'
-        subdir = f'{exposures_path}/exposures/{night}/{expid}'
-        
+        subdir = f'{exposures_path}/{night}/{expid}'
+
         # first the simspec file
         fname = f'simspec-{expid}.fits'
         print(f'reading in {fname}')
         simspec_hdul = fits.open(f'{subdir}/{fname}')
-        simspec_ind = np.where(simspec_hdul[13].data['TARGETID'] == targetid)[0]
+        simspec_ind = np.where(simspec_hdul[simspec_hdul.index_of('FIBERMAP')].data['TARGETID'] == targetid)[0]
         if nrows == 1:
-            ax = axes[0]
+            ax1 = axes1[0]
+            ax2 = axes2[0]
         else:
-            ax = axes[i, 0]
-        ax.plot(simspec_hdul[0].data, simspec_hdul[1].data[simspec_ind, :][0, :])
-        ax.set_title(f'simspec template; ind {simspec_ind}\nnight {night}; expid {expid}', fontsize=14)
+            ax1 = axes1[i, 0]
+            ax2 = axes2[i, 0]
+        ax1.plot(simspec_hdul[0].data, simspec_hdul[1].data[simspec_ind, :][0, :])
+        ax1.set_title(f'simspec template; ind {simspec_ind}\nnight {night}; expid {expid}', fontsize=14)
+        ax2.set_title(f'simspec template; ind {simspec_ind}\nnight {night}; expid {expid}', fontsize=14)
 
-        # now the Spectra files   
+        # now the coadded Spectra files
         fnames = [f for f in os.listdir(subdir) if f.startswith(f'spectra-{petal}-')]
         if len(fnames) != 1:
             raise ValueError(f'somethings weird: have {len(fnames)} Spectra fits files in {subdir}')
@@ -147,57 +248,92 @@ def compare_spectra_groupedcoadded_vs_not(exposures_path, nside,
             spectra = read_spectra(f'{subdir}/{fnames[0]}')
             ind_petal_spec = np.where(spectra.fibermap['TARGETID'] == targetid)[0]
             if nrows == 1:
-                ax = axes[1]
+                ax1 = axes1[1]
+                ax2 = axes2[1]
             else:
-                ax = axes[i, 1]
+                ax1 = axes1[i, 1]
+                ax2 = axes2[i, 1]
             for band in 'brz':
                 nwave = len(spectra.wave[band])
-                ax.plot(spectra.wave[band], spectra.flux[band][ind_petal_spec, :].reshape(nwave))
-            ax.set_title(f'calibrated Spectra; ind {ind_petal_spec}\npetal {petal}; night {night}; expid {expid}', fontsize=14)
-            
+                ax1.plot(spectra.wave[band], spectra.flux[band][ind_petal_spec, :].reshape(nwave))
+                ax2.plot(spectra.wave[band], spectra.ivar[band][ind_petal_spec, :].reshape(nwave))
+            ax1.set_title(f'calibrated Spectra; ind {ind_petal_spec}\npetal {petal}; night {night}; expid {expid}', fontsize=14)
+            ax2.set_title(f'calibrated Spectra ivar; ind {ind_petal_spec}\npetal {petal}; night {night}; expid {expid}', fontsize=14)
+
     grouped = []
     # now lets read the coadded spectrum
-    coadded = read_spectra(f'{path}/{hpixnum}/spectra-{nside}-{hpixnum}.fits')
+    coadded = read_spectra(f'{coadd_spectra_path}/{hpixnum//100}/{hpixnum}/spectra-{nside}-{hpixnum}.fits')
     ind_coadded = np.where(coadded.fibermap['TARGETID'].value == targetid)[0]
     if len(ind_coadded) != 1:
         raise ValueError(f'somethings wrong - expect 1 coadd entry but got {len(ind_coadded)}; targetid = {targetid}; hpix = {hpixnum}')
     else:
         ind = ind_coadded[0]
         if nrows == 1:
-            ax = axes[3]
+            ax1 = axes1[3]
+            ax2 = axes2[3]
         else:
-            ax = axes[0, 3]
+            ax1 = axes1[0, 3]
+            ax2 = axes2[0, 3]
             for i in range(1, nrows):
-                axes[i, 3].axis('off')
+                axes1[i, 3].axis('off')
+                axes2[i, 3].axis('off')
         for band in 'brz':
             nwave = len(coadded.wave[band])
-            ax.plot(coadded.wave[band], coadded.flux[band][ind, :].reshape(nwave))
-            ax.set_title(f'coadded observation; ind {ind}\nflux from coadded-spectra file', fontsize=14)
+            ax1.plot(coadded.wave[band], coadded.flux[band][ind, :].reshape(nwave))
+            ax1.set_title(f'coadded observation; ind {ind}\nflux from coadded-spectra file', fontsize=14)
+
+            ax2.plot(coadded.wave[band], coadded.ivar[band][ind, :].reshape(nwave))
+            ax2.set_title(f'coadded observation; ind {ind}\nivar from coadded-spectra file', fontsize=14)
     # plot details
-    fig.set_size_inches(20, 3*nrows)
-    if nrows <= 2:
-        y = 1.25
+    fig1.set_size_inches(20, 3*nrows)
+    fig2.set_size_inches(20, 3*nrows)
+    if nrows == 1:
+        y = 1.30
+    elif nrows == 2:
+        y = 1.15
     else:
-        y = 1.01
-    plt.suptitle(f'targetID {targetid}\n healpix pixel {hpixnum} (nside {nside})', fontsize=18, y=y, fontweight="bold")  
+        y = 1.06
+
+    title = f'targetID {targetid}\n healpix pixel {hpixnum} (nside {nside})\n'
+    simspec_truth = simspec_hdul[simspec_hdul.index_of('TRUTH')].data
+    for key in ['TRUEZ', 'TRUESPECTYPE', 'TEMPLATETYPE', 'TEMPLATESUBTYPE']:
+        title += f'{key} {simspec_truth[key][simspec_ind]} ; '
+    title += f'simspec-ind {simspec_ind}'
+    fig1.suptitle(title, y=y)
+    fig2.suptitle(title, y=y)
     if nrows == 1:
         for col in range(ncols):
-            axes[col].set_xlabel('wavelength')
-        axes[0].set_ylabel('flux')
+            axes1[col].set_xlabel('wavelength')
+            axes2[col].set_xlabel('wavelength')
+        axes1[0].set_ylabel('flux')
+        axes2[0].set_ylabel('ivar')
     else:
         for col in range(ncols):
-            axes[-1, col].set_xlabel('wavelength')
-        axes[0, -1].set_xlabel('wavelength')
+            axes1[-1, col].set_xlabel('wavelength')
+            axes2[-1, col].set_xlabel('wavelength')
+        axes1[0, -1].set_xlabel('wavelength')
+        axes2[0, -1].set_xlabel('wavelength')
         for row in range(nrows):
-            axes[row, 0].set_ylabel('flux')
+            axes1[row, 0].set_ylabel('flux')
+            axes2[row, 0].set_ylabel('ivar')
+
     # save fig and/or show
     if showfig:
         plt.show()
     if savefig:
+        plt.suptitle(title, y=y)
         fname = f'plot_spectra_template-to-coadd_targetid{targetid}_hpix{hpixnum}_nside{nside}.png'
-        fig.savefig('%s/%s' % (outdir, fname), format='png', bbox_inches='tight')
+        fig1.savefig('%s/%s' % (outdir, fname), format='png', bbox_inches='tight')
         plt.close()
         print('## saved %s' % fname )
+
+        fname = f'plot_spectra-ivar_template-to-coadd_targetid{targetid}_hpix{hpixnum}_nside{nside}.png'
+        fig2.savefig('%s/%s' % (outdir, fname), format='png', bbox_inches='tight')
+        plt.close()
+        print('## saved %s' % fname )
+
+    if return_objdata:
+        return [simspec_ind[0], night, expid]
 
 def plot_delta_attributes(data_path, outdir):
     """
